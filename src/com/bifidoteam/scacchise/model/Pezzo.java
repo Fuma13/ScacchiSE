e6ddd02a6b9b7ab51fc8a99925de4a5eed2e3982
valerik87 <valerio.ceraudo@gmail.com>
2014-12-12 11:55:10 +0100

    Crezione classe Pezzo (non finita)

--- src/com/bifidoteam/scacchise/model/Pezzo.java
+++ src/com/bifidoteam/scacchise/model/Pezzo.java
@@ -1,5 +1,39 @@
 package com.bifidoteam.scacchise.model;
 
-public class Pezzo {
 
+public class Pezzo {
+	
+	//private Variables
+	private int index;
+	private boolean color;
+	//private Graphic_Info gi; 								-> creare la classe Graphic_Info
+	
+	//Costructors 
+	public Pezzo(int startingIndex,boolean startingColor){
+		this.index = startingIndex;
+		this.color = startingColor;
+	}
+	
+	
+	
+	//public functions
+	//public MedusaTree GetReachableIndices();				-> creare la classe MedusaTreee
+	
+	
+	//getter/setter
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	public boolean isColor() {
+		return color;
+	}
+
+	public void setColor(boolean color) {
+		this.color = color;
+	}
 }
