aa737f3ecafb016b630f1424fcc4a3b599a356b9
valerik87 <valerio.ceraudo@gmail.com>
2014-12-12 12:07:21 +0100

    pardon alcune modifiche.... non vedevo un abstract da secoli

--- src/com/bifidoteam/scacchise/model/Pezzo.java
+++ src/com/bifidoteam/scacchise/model/Pezzo.java
@@ -4,8 +4,8 @@ package com.bifidoteam.scacchise.model;
 public class Pezzo {
 	
 	//private Variables
-	private int index;
-	private boolean color;
+	protected int index;
+	protected boolean color;
 	//private Graphic_Info gi; 								-> creare la classe Graphic_Info
 	
 	//Costructors 
@@ -13,27 +13,24 @@ public class Pezzo {
 		this.index = startingIndex;
 		this.color = startingColor;
 	}
-	
-	
-	
+		
 	//public functions
 	//public MedusaTree GetReachableIndices();				-> creare la classe MedusaTreee
 	
 	
-	//getter/setter
+	//--------------------------------getter/setter-----------------------------------------
 	public int getIndex() {
 		return index;
 	}
-
 	public void setIndex(int index) {
 		this.index = index;
 	}
 
-	public boolean isColor() {
+	public boolean getColor() {
 		return color;
 	}
-
 	public void setColor(boolean color) {
 		this.color = color;
 	}
+	//--------------------------------getter/setter-----------------------------------------
 }
