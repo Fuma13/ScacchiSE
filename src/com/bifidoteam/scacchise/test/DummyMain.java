350dd8b7ab22190caca3c5be91a833eb3b15af35
Riccardo <ricky.stucchi@gmail.com>
2015-03-02 16:59:53 +0100

    Added some basic logics to dummy main

--- src/com/bifidoteam/scacchise/test/DummyMain.java
+++ src/com/bifidoteam/scacchise/test/DummyMain.java
@@ -1,11 +1,33 @@
 package com.bifidoteam.scacchise.test;
 
+import com.bifidoteam.scacchise.util.Constants;
+import com.bifidoteam.util.MedusaTree;
+
 public class DummyMain {
 
 	public static void main(String[] args) {
 		
-		System.out.print("TEST");
+		printMedusa(null);
 		
+	}
+	
+	private static void printMedusa(MedusaTree toPrint){
+		
+		// TODO: iter on the iterator and  call writeChar() to create the tree
+
+		for(int i=0; i< Constants.MAX_INDEX_ROW; i++)
+			writeWhiteLine();
+	}
+	
+	private static void writeChar(char c){
+		System.out.print("[" + c + "]");
+	}
+	
+	private static void writeWhiteLine(){
+		for(int i=0; i< Constants.MAX_INDEX_ROW; ++i){
+			writeChar(' ');
+		}
+		System.out.println();
 	}
 
 }
