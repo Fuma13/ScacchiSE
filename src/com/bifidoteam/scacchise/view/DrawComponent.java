73a2788eca0c4fe6795943bd0efadc06f894149f
Riccardo <ricky.stucchi@gmail.com>
2015-03-10 11:45:50 +0100

    Implemented most of the view functionality

--- src/com/bifidoteam/scacchise/view/DrawComponent.java
+++ src/com/bifidoteam/scacchise/view/DrawComponent.java
@@ -2,13 +2,15 @@ package com.bifidoteam.scacchise.view;
 
 import java.util.Iterator;
 
+import com.bifidoteam.scacchise.model.Chessboard;
 import com.bifidoteam.scacchise.model.Piece;
 import com.bifidoteam.scacchise.util.Constants;
 import com.bifidoteam.util.MedusaTree;
 
 public class DrawComponent {
 
-	private char[] map = new char[Constants.MAX_INDEX];
+	private Chessboard scacchiera;
+	//private char[] map = new char[Constants.MAX_INDEX];
 	private char[] medusaMap = new char[Constants.MAX_INDEX];
 	
 
@@ -27,12 +29,18 @@ public class DrawComponent {
 		System.out.println();
 		// ** END
 		
+		char actualSymbol = ' ';
+		
 		for(int i=0; i<Constants.MAX_INDEX_ROW; ++i){
 			
 			System.out.print(" " + i + " ");
 			
 			for(int j=0; j<Constants.MAX_INDEX_ROW; ++j){
-				writeChar(map[i*Constants.MAX_INDEX_ROW + j]);
+				actualSymbol = scacchiera.GetPiece(i*Constants.MAX_INDEX_ROW + j).GetSymbol();
+				if(medusaMap[i*Constants.MAX_INDEX_ROW + j] == 'X')
+					writeChar(actualSymbol);
+				else
+					writeSpecialChar(actualSymbol);
 			}
 			System.out.println();
 		}
@@ -41,15 +49,16 @@ public class DrawComponent {
 		initMedusaMap(' ');
 	}
 
-	public DrawComponent(){
-		initMap(' ');
+	public DrawComponent(Chessboard chessboardIn){
+		//initMap(' ');
+		scacchiera = chessboardIn;
 		initMedusaMap(' ');
 	}
 	
-	public void writePieceOnMap(int pos, Piece x){
-		if(pos >= 0 && pos < Constants.MAX_INDEX)
-			map[pos] = (char) (x.isWhite() == 0 ? x.GetSymbol(): (x.GetSymbol() +32));
-	}
+//	public void writePieceOnMap(int pos, Piece x){
+//		if(pos >= 0 && pos < Constants.MAX_INDEX)
+//			map[pos] = (char) (x.isWhite() == 0 ? x.GetSymbol(): (x.GetSymbol() +32));
+//	}
 	
 	public void writeMedusaOnMap(MedusaTree informations, boolean completed){
 		
@@ -80,11 +89,6 @@ public class DrawComponent {
 		F,
 		G,
 		H
-	}
-	
-	private void initMap(char initValue){
-		for(int i=0; i< map.length; ++i)
-			map[i] = initValue;
 	}
 	
 	private void initMedusaMap(char initValue){
--- src/com/bifidoteam/scacchise/view/GameConsoleView.java
+++ src/com/bifidoteam/scacchise/view/GameConsoleView.java
@@ -1,50 +1,60 @@
 package com.bifidoteam.scacchise.view;
 
 import com.bifidoteam.scacchise.controller.GameManager;
+import com.bifidoteam.scacchise.interfaces.ControllerInterface;
 import com.bifidoteam.scacchise.interfaces.ViewInterface;
 import com.bifidoteam.scacchise.model.Chessboard;
 import com.bifidoteam.util.MedusaTree;
 
 public class GameConsoleView implements ViewInterface {
 
-	Chessboard toRender; //TODO: Creare un drawcomponent con al posto di map la chessboard
+	DrawComponent drawingClass;
+	ControllerInterface gm;
+	
+	public GameConsoleView() {
+		gm = GameManager.getInstance();
+	}
 	
 	@Override
 	public void Init(Chessboard base) {
-		toRender = base;
+		drawingClass = new DrawComponent(base);
 	}
 	
 	@Override
 	public void Render(MedusaTree reacheblePosition) {
-		// TODO Auto-generated method stub
+		drawingClass.writeMedusaOnMap(reacheblePosition, false);
 		
+		drawingClass.printMap("Test print:");
 	}
 
 	@Override
 	public void MoveFromStartIndexToEndIndex(int startIndex, int endIndex) {
 		
-		GameManager.getInstance().OnMoveDone();
+		gm.OnMoveDone();
 		
 		return;
 	}
 
-//	@Override
-//	public void OnConfigurationChange() {
-//		// TODO Auto-generated method stub
-//		
-//	}
-
 	@Override
 	public int GetInput() {
-		return 0;
-		// TODO Auto-generated method stub
 		
+		System.out.println("Write next Move: (Syntax: \"king\" or \"A4\")");
+		String in = System.console().readLine();
+		
+		return ParseInput(in);
 	}
 
 	@Override
 	public void EndGame(int info) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("CheckMate!!! Player " + (info == 0 ? "White" : "Black") + " Win!!");
+	}
+	
+	
+	
+	
+	private int ParseInput(String input){
+		// TODO: Implement the parser
+		return 0;
 	}
 
 }
